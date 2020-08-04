987192528b3363c3e3ed49aa99b3db343f38574c
Monica Rangel <monicarangel95@github.com>
2020-08-03 21:31:44 -0700

    update employee-tracker.js -allows user to view all employees
    Retrieving employees from database

--- employee-tracker.js
+++ employee-tracker.js
@@ -56,7 +56,7 @@ const askQuestion = function () {
         });
 };
 askQuestion();
-// Allows user to view all departments currently in the database
+// Allows user to VIEW ALL DEPARTMENTS currently in the database
 function viewalldepartments() {
     connection.query("SELECT * FROM department", function (err, answer) {
         console.log("\n Departments Retrieved from Database \n");
@@ -64,10 +64,21 @@ function viewalldepartments() {
     });
     askQuestion();
 }
-// Allows user to view all employee roles currently in the database
+// Allows user to VIEW ALL ROLES currently in the database
 function viewallroles() {
     connection.query("SELECT * FROM role", function (err, answer) {
         console.log("\n Roles Retrieved from Database \n");
+        console.table(answer);
+    });
+    askQuestion();
+}
+// Allows user to VIEW ALL EMPLOYEES currently in the database
+function viewallemployees() {
+    console.log("Employees Retrieved from database");
+    var fancyQuery =
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id;";
+    connection.query(fancyQuery, function (err, answer) {
+        console.log("\n Employees retrieved from Database \n");
         console.table(answer);
     });
     askQuestion();
